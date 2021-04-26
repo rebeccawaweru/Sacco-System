55773a7bea0e3e849f162506fccc455fcc4ad48e
rebeccawaweru <wawerur95@gmail.com>
2021-04-26 22:13:08 +0300

    update

---
+++ js/admin.js
@@ -0,0 +1,30 @@
+document.getElementById("add").onclick =function(){
+
+    document.getElementById("content").style.display="none";
+
+    document.getElementById("content2").style.display= "block";
+
+}
+
+document.getElementById("dash").onclick =function(){
+
+    document.getElementById("content").style.display="block";
+
+    document.getElementById("content2").style.display= "none";
+
+}
+
+document.getElementById("member").onclick = function(){
+
+    document.getElementById("content").style.display= "none";
+    document.getElementById("content2").style.display="none";
+    document.getElementById("content3").style.display="block";
+    
+
+   
+}
+//flightbooking 
+//getting a list
+
+
+
